fd3caf0d5d29fec0f0b824f1609e99690c2b120a
e-haghparast <73380762+e-haghparast@users.noreply.github.com>
2023-01-19 22:26:23 +0330

    created react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        This project was coded by Elnaz Haghparast and is open-sourced on {" "}
+        <a href="https://github.com/e-haghparast">GitHub</a>
+      </p>
     </div>
   );
 }
-
-export default App;
