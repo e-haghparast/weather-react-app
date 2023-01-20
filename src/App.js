22a8ed732fbdd796188b301af2dee480720b9feb
e-haghparast <73380762+e-haghparast@users.noreply.github.com>
2023-01-20 10:58:37 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <p>
         This project was coded by Elnaz Haghparast and is open-sourced on {" "}
         <a href="https://github.com/e-haghparast">GitHub</a>
       </p>
+      </div>
     </div>
   );
 }
